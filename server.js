ee5ed8fe30bac01c4c731a333d436714b3f496fa
Osuide clement <debaosuidecl@gmail.com>
2020-03-18 23:16:11 +0100

    D

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get("/ping/:cid", async (req, res) => {
   let redirectDetails = await ACCESS_HOST(req.params.cid, req.query.traffic, req.query.redirect)
 
   const {traffic, title, redirectLink,customer} = JSON.parse(redirectDetails);
-  console.log(redirectDetails)
+  console.log(JSON.parse(redirectDetails))
   res.render("redirectclickers.ejs", {
     traffic,
     title,
