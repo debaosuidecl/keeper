ec3d72458fa909ffad37fb321ac8003f3652a81e
Osuide clement <debaosuidecl@gmail.com>
2020-04-09 15:18:24 +0100

    D

--- server.js
+++ server.js
@@ -41,7 +41,12 @@ app.listen(PORT, function() {
 
 
 app.get("/ip-test", async (req,res)=> {
-  console.log(req.headers['X-Forwarded-For'], "NNNNNNNNNNNNNNNNNNNAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+
+var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+  console.log(ip, "NNNNNNNNNNNNNNNNNNNAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
 })
 
   app.get("/pingmeta/:cid", async (req, res) => {
